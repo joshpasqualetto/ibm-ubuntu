a672285c36ef40cbff73ccf52a68834a476ea7bb
Josh Pasqualetto <josh.pasqualetto@sonian.net>
2012-02-21 13:07:09 -0800

    Full dir w/ shasum

--- solo.rb
+++ solo.rb
@@ -1,3 +1,3 @@
 file_cache_path "/var/chef-solo"
-cookbook_path "/var/chef-solo/sniperd-ibm-ubuntu-*"
+cookbook_path "/var/chef-solo/sniperd-ibm-ubuntu-2f78cbc"
 recipe_url "https://github.com/sniperd/ibm-ubuntu/tarball/master"
