217e393bafd60c79135bc5f540d4b094cb90d3db
Josh Pasqualetto <josh.pasqualetto@sonian.net>
2012-02-21 13:05:54 -0800

    Test to see if globbing is allowed

--- solo.rb
+++ solo.rb
@@ -1,3 +1,3 @@
 file_cache_path "/var/chef-solo"
-cookbook_path "/var/chef-solo/cookbooks"
+cookbook_path "/var/chef-solo/sniperd-ibm-ubuntu-*"
 recipe_url "https://github.com/sniperd/ibm-ubuntu/tarball/master"
