13e0cb5115eb5cef0b9cf33c19e98598908a9f2a
Josh Pasqualetto <josh.pasqualetto@sonian.net>
2012-02-21 13:48:00 -0800

    Reorganize compressed files

--- solo.rb
+++ solo.rb
@@ -1,4 +1,4 @@
 file_cache_path "/var/chef-solo"
 cookbook_path "/var/chef-solo/cookbooks"
 json_attribs "https://raw.github.com/sniperd/ibm-ubuntu/master/node.json"
-recipe_url "https://github.com/sniperd/ibm-ubuntu/blob/master/cookbooks.tgz"
+recipe_url "https://github.com/sniperd/ibm-ubuntu/raw/master/compressed/cookbooks.tgz"
