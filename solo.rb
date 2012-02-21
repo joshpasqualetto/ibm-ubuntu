3f0543e0d5924269451542a3cef584236fb14800
Josh Pasqualetto <josh.pasqualetto@sonian.net>
2012-02-21 13:01:25 -0800

    Use a tarball instead of zip

--- solo.rb
+++ solo.rb
@@ -1,3 +1,3 @@
 file_cache_path "/var/chef-solo"
 cookbook_path "/var/chef-solo/cookbooks"
-recipe_url "https://github.com/sniperd/ibm-ubuntu/zipball/master"
+recipe_url "https://github.com/sniperd/ibm-ubuntu/tarball/master"
