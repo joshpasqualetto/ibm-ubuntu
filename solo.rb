9de3ce32b41a18c2eb4ba0c21ab11bc648e9e690
Josh Pasqualetto <josh.pasqualetto@sonian.net>
2012-02-21 13:21:00 -0800

    Add json attribs to solo

--- solo.rb
+++ solo.rb
@@ -1,3 +1,4 @@
 file_cache_path "/var/chef-solo"
 cookbook_path "/var/chef-solo/sniperd-ibm-ubuntu-2f78cbc"
+json_attribs "https://raw.github.com/sniperd/ibm-ubuntu/master/node.json"
 recipe_url "https://github.com/sniperd/ibm-ubuntu/tarball/master"
